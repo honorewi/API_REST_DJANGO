5663712a06b1522fa7c675e00694d9f8cbb4bc23
honorewi <hsemdi@gmail.com>
2024-09-21 20:34:39 +0100

    initial commit

---
+++ project/urls.py
@@ -0,0 +1,25 @@
+from django.contrib import admin
+from django.urls import path, include
+
+from rest_framework import routers
+ 
+from shop.views import CategoryViewset
+from shop.views import ProductViewset
+from shop.views import ArticleViewset
+from rest_framework import routers
+from django.contrib import admin
+from django.urls import path, include
+
+from shop.views import CategoryViewset, ProductViewset, ArticleViewset
+
+
+router = routers.SimpleRouter()
+router.register('category', CategoryViewset, basename='category')
+router.register('product', ProductViewset, basename='product')
+router.register('article', ArticleViewset, basename='article')
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls')),
+    path('api/', include(router.urls))
+]
